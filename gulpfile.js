de866bf216cad90b7cc9aece3c32ca9e44cabda8
Brian Norris <bdnorris@gmail.com>
2019-06-25 21:27:25 -0500

    Upgrade to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,67 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-// var useref = require('gulp-useref');
-// var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-// var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
-var browserSync = require('browser-sync');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("gulp-cssnano");
+var browserSync = require("browser-sync").create();
 
-// Development Tasks
-// -----------------
+const path = {
+  sass: "scss/**/*.scss",
+  html: "**/*.html"
+};
 
-// Start browserSync server
-gulp.task('browserSync', function() {
-  browserSync({
+// Styles task for production `gulp styles`
+gulp.task("styles", function() {
+  return gulp
+    .src(path.sass) // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass().on("error", sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: [">1%"],
+          cascade: false,
+          grid: true
+        })
+      ])
+    )
+    .pipe(cssnano({ reduceIdents: false })) // this helps prevent breaking animations // for mini-fying CSS, leaving off for now
+    .pipe(gulp.dest(".")); // Outputs it in the root folder
+});
+
+// Styles task for development with sourcemaps `gulp dev`
+gulp.task("sass", function() {
+  return gulp
+    .src(path.sass) // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sourcemaps.init()) // Init sourcemaps
+    .pipe(sass().on("error", sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(sourcemaps.write()) // Write it, it's embedded, making the file much larger. Should be turned off for Production
+    .pipe(gulp.dest(".")); // Outputs it in sibling CSS folder
+});
+
+// Watch task
+// gulp.task("watch", function() {
+//   gulp.watch(path.sass, gulp.series("sass"));
+// });
+
+// Build `gulp build`
+// Styles then stop
+gulp.task("build", gulp.series("styles"));
+
+// Static Server + watching scss/html files
+gulp.task("serve", function() {
+  browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: "./"
     }
-  })
-})
+  });
 
-gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(autoprefixer({
-    	browsers: ['last 2 versions'],
-    	cascade: false
-    })) // autoprefixer
-    // .pipe(cssnano()) // for mini-fying CSS, leaving off for now
-    .pipe(browserSync.reload({
-      stream: true
-  }))
-    .pipe(gulp.dest('')) // Outputs it in the root folder
-})
+  gulp.watch(path.sass, gulp.series("sass"))
+    .on("change", browserSync.reload);
+  gulp.watch(path.html)
+    .on("change", browserSync.reload);
+});
 
-// Watchers
-gulp.task('watch', ['browserSync', 'sass'], function() {
-  gulp.watch('scss/**/*.scss', ['sass'], browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
-})
-
-// Optimization Tasks
-// ------------------
-
-// Optimizing CSS
-//gulp.task('useref', function() {
-//  return gulp.src('app/*.html')
-//    .pipe(useref())
-//    .pipe(gulpIf('*.css', cssnano()))
-//});
-
-
-// Build Sequences
-// ---------------
-
-gulp.task('default', function(callback) {
-  runSequence(['sass'], 'watch',
-    callback
-  )
-})
-
-//gulp.task('build', function(callback) {
-//  runSequence(
-//    'sass',
-//    callback
-//  )
-//})
+// Default `gulp`
+// Styles then watch
+gulp.task("default", gulp.series("sass", "serve"));
